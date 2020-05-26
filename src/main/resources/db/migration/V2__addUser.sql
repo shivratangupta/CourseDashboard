fe60bbaa35b00ef6ecbffd49a6a0cc6482b276e5
shivratangupta <shivratangupta18@gmail.com>
2020-05-27 03:06:46 +0530

    add thymleaf dependencies and rename adduser migration

--- src/main/resources/db/migration/V2__addUser.sql
+++ src/main/resources/db/migration/V2__addUser.sql
@@ -1,7 +1,8 @@
-Create TABLE Users (
+CREATE TABLE Users (
     ID int,
     LastName varchar(255),
     FirstName varchar(255),
     Address varchar(255),
-    City varchar(255)
+    City varchar(255),
+    PRIMARY KEY (ID)
 );
